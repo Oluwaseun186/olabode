63fa3af1898ba2a96b805d4153803a695442ae32
oluwaseun186 <adewumibode7@gmail.com>
2025-05-18 00:19:37 +0100

    addded env 2

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Serve static files - THIS IS CRUCIAL
+// Serve static files -
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/js', express.static(path.join(__dirname, 'public', 'js')));
 app.use('/css', express.static(path.join(__dirname, 'public', 'css')));
-// Routes
+
+
+
 app.get('/', (req, res) => {
     res.render('index');
 });
