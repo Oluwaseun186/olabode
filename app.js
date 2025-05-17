384a70096960674e7e80a4aeeafec1366ff00854
oluwaseun186 <adewumibode7@gmail.com>
2025-05-18 00:08:38 +0100

    addded env

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-const PORT = 7000;
+const PORT = process.env.PORT 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
